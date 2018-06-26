cc30ed9ee3369d934fcaef101ca8d98fb7e52529
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-06-26 05:35:42 +0300

    blog microservice

--- gateway/src/main/webapp/app/entities/entity.module.ts
+++ gateway/src/main/webapp/app/entities/entity.module.ts
@@ -2,6 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GatewaySpeakerModule as ConferenceSpeakerModule } from './conference/speaker/speaker.module';
 import { GatewaySessionModule as ConferenceSessionModule } from './conference/session/session.module';
+import { GatewayBlogModule as BlogBlogModule } from './blog/blog/blog.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -9,6 +10,7 @@ import { GatewaySessionModule as ConferenceSessionModule } from './conference/se
     imports: [
         ConferenceSpeakerModule,
         ConferenceSessionModule,
+        BlogBlogModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
